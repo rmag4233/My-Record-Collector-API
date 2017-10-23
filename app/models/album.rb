d9041f7057c5e9e4ccf8fb5268d664e324a41434
rmag4233 <ryan.agnew@libertymutual.com>
2017-10-23 12:53:21 -0400

    Add validation in album model
    Add validation in album model to ensure presence of certain
    form fields

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,8 @@
 class Album < ApplicationRecord
   belongs_to :user
   validates :user, presence: true
+  validates :title, presence: true
+  validates :artist_name, presence: true
+  validates :year_released, presence: true
+  validates :format, presence: true
 end
